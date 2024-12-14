ee87aa75dd8124282065d5e9554ddd9521c47bb6
Ziyad Patel <zydlmnop@gmail.com>
2024-12-14 19:24:05 +0000

    Initial Commit
    Created an exercise model

---
+++ Models/Exercise.cs
@@ -0,0 +1,12 @@
+﻿namespace GymAppReal.Models
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string ExerciseName { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public double Weight { get; set; }
+        public DateOnly Date { get; set; }
+    }
+}
