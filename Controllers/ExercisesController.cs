2a35e10bab26abd5de7c01e19b873772553e5378
Ziyad Patel <zydlmnop@gmail.com>
2024-12-15 01:37:35 +0000

    GYM2 - Added Controller
    - Add Exercises Controller

---
+++ Controllers/ExercisesController.cs
@@ -0,0 +1,89 @@
+﻿using GymAppReal.Data;
+using GymAppReal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAppReal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly GymContext _context;
+
+        public ExercisesController(GymContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Exercise>> GetExercise(int id)
+        {
+            var exercise = await _context.Exercises.FindAsync(id);
+
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return exercise;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Exercise>> PostExercise(Exercise exercise)
+        {
+            _context.Exercises.Add(exercise);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetExercise), new { id = exercise.Id }, exercise);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExercise(int id, Exercise exercise)
+        {
+            if (id != exercise.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(exercise).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExerciseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExercise(int id)
+        {
+            var exercise = await _context.Exercises.FindAsync(id);
+            if (exercise == null)
+            {
+                return NotFound();
+            } 
+
+            _context.Exercises.Remove(exercise);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ExerciseExists(int id)
+        {
+            return _context.Exercises.Any(e => e.Id == id);
+        }
+    }
+}
